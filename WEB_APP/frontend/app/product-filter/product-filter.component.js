a66cfea333e7826a5ff505a364048c655c8c2705
MaryLukyanova <bobbkrist@gmail.com>
2017-12-21 09:33:13 +0300

    filter a lil bit

--- WEB_APP/frontend/app/product-filter/product-filter.component.js
+++ WEB_APP/frontend/app/product-filter/product-filter.component.js
@@ -6,8 +6,8 @@ component('productFilter', {
     templateUrl: 'product-filter/product-filter.template.html',
     controller: ['$http', function ProductListController($http) {
         var self = this;
-        $http.get('products/products.json').then(function(response) {
-            self.products = response.data;
+        $http.get('filters/mobile.json').then(function(response) {
+            self.filters = response.data;
         });
     }]
 });
