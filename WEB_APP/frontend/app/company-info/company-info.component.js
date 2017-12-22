b58e51ef1fe3d2762a36a32765e9dd60a7f08f3f
MaryLukyanova <bobbkrist@gmail.com>
2017-12-23 01:41:45 +0300

    company info

--- WEB_APP/frontend/app/company-info/company-info.component.js
+++ WEB_APP/frontend/app/company-info/company-info.component.js
@@ -4,10 +4,26 @@ angular.
 module('companyInfo').
 component('companyInfo', {
     templateUrl: 'company-info/company-info.template.html',
-    controller: ['$http', function ProductListController($http) {
-        var self = this;
-        $http.get('products/products.json').then(function(response) {
-            self.products = response.data;
-        });
-    }]
+    controller: ['$http', '$routeParams',
+        function CompanyInfoController($http, $routeParams) {
+            var self = this;
+            var info = $routeParams.infoCategory;
+            this.aboutcompany='';
+            this.sales='';
+            this.payment='';
+            this.delivery='';
+            this.contacts='';
+
+            if(info == 'aboutcompany')
+                this.aboutcompany = true;
+            else if(info == 'sales')
+                this.sales = true;
+            else if(info == 'payment')
+                this.payment = true;
+            else if(info == 'delivery')
+                this.delivery = true;
+            else if(info == 'contacts')
+                this.contacts = true;
+        }
+    ]
 });
--- WEB_APP/frontend/app/company-info/company-info.module.js
+++ WEB_APP/frontend/app/company-info/company-info.module.js
@@ -1,3 +1,5 @@
 'use strict';
 
-angular.module('companyInfo', []);
+angular.module('companyInfo', [
+    'ngRoute'
+]);
