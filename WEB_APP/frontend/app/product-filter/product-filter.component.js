d6a607bbdb6ad70e518b124ea72704eae354abc7
MaryLukyanova <bobbkrist@gmail.com>
2017-12-22 23:01:15 +0300

    filter all ready

--- WEB_APP/frontend/app/product-filter/product-filter.component.js
+++ WEB_APP/frontend/app/product-filter/product-filter.component.js
@@ -7,9 +7,32 @@ component('productFilter', {
     controller: ['$http', '$routeParams',
         function ProductFilterController($http, $routeParams) {
             var self = this;
-            $http.get('https://digital-app.herokuapp.com/products_by_category?category=' + $routeParams.filterCategory).then(function(response) {
-                self.filters = response.data;
+            var ctg =  $routeParams.filterCategory;
+            $http.get('https://digital-app.herokuapp.com/products_by_category?category='+ctg).then(function(response) {
+                self.filterproducts = response.data;
             });
+            self.formInfo = {};
+            self.filterData = function(formInfo) {
+                var priceFrom = formInfo.priceFrom;
+                var priceTo = formInfo.priceTo;
+                var manufacturer = formInfo.manufacturer;
+                if(priceFrom == undefined){
+                    priceFrom = '';
+                }if(priceTo == undefined){
+                    priceTo = '';
+                }if(manufacturer == undefined){
+                    manufacturer = '';
+                }
+
+                $http.get('https://digital-app.herokuapp.com/filter?category='+ctg+'&brand='+manufacturer+'&from='+priceFrom+'&to='+priceTo).then(function(response) {
+                    self.filterproducts = response.data;
+                });
+            };
+            self.resetData = function() {
+                $http.get('https://digital-app.herokuapp.com/products_by_category?category='+ctg).then(function(response) {
+                    self.filterproducts = response.data;
+                });
+            };
         }
     ]
 });
--- WEB_APP/frontend/app/product-list/product-list.component.js
+++ WEB_APP/frontend/app/product-list/product-list.component.js
@@ -4,10 +4,19 @@ angular.
 module('productList').
 component('productList', {
     templateUrl: 'product-list/product-list.template.html',
-    controller: ['$http', function ProductListController($http) {
+    controller: ['$http', '$routeParams',
+        function ProductListController($http, $routeParams) {
         var self = this;
         $http.get('https://digital-app.herokuapp.com/products').then(function(response) {
             self.products = response.data;
         });
+
+        self.searchData = function(formSearch) {
+            self.hellot = formSearch.textSearch;
+            $http.get('filters/mobile.json').then(function(response) {
+                self.products = response.data;
+            });
+        };
+
     }]
 });
